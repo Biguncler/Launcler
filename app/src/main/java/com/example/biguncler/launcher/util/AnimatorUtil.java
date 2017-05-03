1cc51833e5c5faa0681b6fa154278cd0852dca16
ex-weijunshu001 <ex-weijunshu001@pingan.com.cn>
2017-05-03 16:49:28 +0800

    新增AnimatorUtil类

--- app/src/main/java/com/example/biguncler/launcher/util/AnimatorUtil.java
+++ app/src/main/java/com/example/biguncler/launcher/util/AnimatorUtil.java
@@ -12,6 +12,17 @@ import android.view.View;
  */
 
 public class AnimatorUtil {
+    public static final String SCALE="scale";
+    public static final String SCALE_X="scaleX";
+    public static final String SCALE_Y="scaleY";
+    public static final String ALPHA="alpha";
+    public static final String ROTATION="rotation";
+    public static final String ROTATION_X="rotationX";
+    public static final String ROTATION_Y="rotationY";
+    public static final String TRANSLATION_X="translationX";
+    public static final String TRANSLATION_Y="translationY";
+    public static final String TRANSLATION="translation";
+
 
     public void startAnimator(View target, String propertyName, float start, float end, float pivotX, float pivotY,int duration, TimeInterpolator timeInterpolator, AnimatorListenerAdapter listenerAdapter){
         if(pivotX>=0)target.setPivotX(pivotX);
